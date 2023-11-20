2e1cd837874445062a746cc65a54b981741aa8d1
Nyukeit <nyukeit@outlook.com>
2023-11-20 15:46:13 +0100

    [ADD] Using Props to pass Pokemons

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,25 +1,13 @@
 import './cards.css';
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-const PokemonCard = () => {
-  let pokemon = pokemonList[1]; // 0 for Bulbasaur, 1 for Mew
-
+const PokemonCard = ({name, imgSrc}) => {
+  // console.log({name, imgSrc});
   return (
     <figure className="card">
       {
-        pokemon.imgSrc ? <img className='cardImg' src={pokemon.imgSrc} alt={pokemon.name}/> : <p>???</p> // Display image if Bulbasaur, ??? if mew
+        imgSrc ? <img className='cardImg' src={imgSrc} alt={name}/> : <p>???</p> // Display image if Bulbasaur, ??? if mew
       }
-      <figcaption>{pokemon.name}</figcaption>
+      <figcaption>{name}</figcaption>
     </figure>
   )
 }
